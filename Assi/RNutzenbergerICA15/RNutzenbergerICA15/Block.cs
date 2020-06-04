aa7b2cf6a1c9f4dabaa0328cb057424eeccf1f1d
rnutzenberger <66440702+rnutzenberger@users.noreply.github.com>
2020-06-04 14:50:30 -0600

    Add files via upload

---
+++ Assi/RNutzenbergerICA15/RNutzenbergerICA15/Block.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+using RNutzenbergerDrawers;
+
+namespace RNutzenbergerICA15
+{
+    class Block
+    {
+        protected RectangleF _rect;
+        protected static Random _RND = new Random();
+
+        public bool Outside { get; private set; }
+
+
+        public Block(PointF point)
+        {
+            _rect.Location = point;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Block arg)) return false;
+            if (ReferenceEquals(this, arg)) return false;
+
+            return _rect.IntersectsWith(arg._rect);
+        }
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public virtual void Move(List<Block> _lBlocks) 
+        {
+
+        }
+
+        public virtual void ShowBlock(CDrawer canvas)
+        {
+           
+            
+            RectangleF temp = _rect;
+            temp.Inflate(3, 3);
+
+            canvas.AddRectangle((int)temp.X, (int)temp.Y, (int)temp.Width, (int)temp.Height, Color.Black);
+            if (_rect.X + _rect.Width/2 > canvas.ScaledWidth || _rect.Y + _rect .Height/2 > canvas.ScaledHeight)
+            {
+                Outside = true;
+                return;
+            }
+        }
+
+
+    }
+
+    class FallingBlock : Block
+    {
+        const float _fVel = 6;
+
+        public FallingBlock(PointF point) : base(point)
+        {
+            _rect.Width = 50;
+            _rect.Height = 50;
+        }
+
+        public override void Move(List<Block> _lBlocks)
+        {
+
+            if (!Outside)
+            {
+                if(!_lBlocks.Where((b)=> !ReferenceEquals(b,this)).Contains(this))
+                {
+                    _rect.Y += _fVel;
+                }
+            }
+        }
+
+        public override void ShowBlock(CDrawer canvas)
+        {
+            base.ShowBlock(canvas);
+            canvas.AddRectangle((int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height, Color.White);
+        }
+    }
+
+    class DrunkBlock : Block
+    {
+        const float _fVel = 3;
+        float _fLatVel = 0;
+
+        public DrunkBlock(PointF point) : base(point)
+        {
+            _rect.Width = 30;
+            _rect.Height = 60;
+        }
+
+        public override void Move(List<Block> _lBlocks)
+        {
+            if (!Outside)
+            {
+                
+                if (!_lBlocks.Where((b) => !ReferenceEquals(b, this)).Contains(this))
+                {
+                    _rect.Y += _fVel;
+
+                    _fLatVel = (float)_RND.NextDouble() * 4 - 2;
+                    _rect.X += _fLatVel;
+                }
+            }
+        }
+
+        public override void ShowBlock(CDrawer canvas)
+        {
+            base.ShowBlock(canvas);
+            canvas.AddRectangle((int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height, Color.Pink);
+        }
+    }
+
+    class ColourBlock : FallingBlock
+    {
+        const float _fVel = 3;
+        private Color _col = RandColor.GetColor();
+
+        public ColourBlock(PointF point) : base(point)
+        {
+            _rect.Width = 60;
+            _rect.Height = 30;
+        }
+
+        public override void Move(List<Block> _lBlocks)
+        {
+            base.Move(_lBlocks);
+            if ((_col.A - _fVel) >= 0)
+            {
+                _col = Color.FromArgb(_col.A - (int)_fVel, _col);
+            }
+
+        }
+
+        public override void ShowBlock(CDrawer canvas)
+        {
+            base.ShowBlock(canvas);
+            
+            canvas.AddRectangle((int)_rect.X, (int)_rect.Y, (int)_rect.Width, (int)_rect.Height, _col);
+
+        }
+    }
+
+}
---
+++ Assi/RNutzenbergerICA15/RNutzenbergerICA15/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using RNutzenbergerDrawers;
+using System.Threading;
+
+namespace RNutzenbergerICA15
+{
+    public partial class Form1 : Form
+    {
+        PicDrawer _pCanvas = null;
+        Keys input;
+        List<Block> _LBlocks = new List<Block>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _pCanvas = new PicDrawer(Properties.Resources.owen_wilson);
+            
+            _pCanvas.ContinuousUpdate = false;
+            _pCanvas.KeyboardEvent += _pCanvas_KeyboardEvent;
+            KeyDown += Form1_KeyDown;
+            Load += Form1_Load;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _pCanvas.AlignToForm(this);
+
+            Thread _threadMove = new Thread(MoveThread);
+            _threadMove.IsBackground = true;
+            _threadMove.Start();
+
+            Thread _threadShow = new Thread(RenderThread);
+            _threadShow.IsBackground = true;
+            _threadShow.Start();
+
+            Thread _threadInput = new Thread(InputThread);
+            _threadInput.IsBackground = true;
+            _threadInput.Start();
+        }
+
+        //thread calls move
+        private void MoveThread()
+        {
+            while (true)
+            {
+                lock (_LBlocks)
+                {
+                    _LBlocks.ForEach((b) => b.Move(_LBlocks));
+                }
+                Thread.Sleep(100);
+            }
+        }
+        
+        //thread calls the SHowBlock
+        private void RenderThread()
+        {
+            while (true)
+            {
+                _pCanvas.Clear();
+                lock(_LBlocks)
+                {
+                    _LBlocks.ForEach((b) => b.ShowBlock(_pCanvas));
+                }
+                _pCanvas.Render();
+                Thread.Sleep(50);
+            }
+        }
+
+        //thread to control our input
+        private void InputThread()
+        {
+            
+
+            while (true)
+            {
+                
+                
+                if (input != Keys.None)
+                {
+                    _pCanvas.GetLastMousePosition(out Point p);
+
+                    if(input == Keys.F)
+                    {
+                        lock (_LBlocks)
+                        {
+                            _LBlocks.Add(new FallingBlock(p));
+                        }
+                    }
+
+                    if(input == Keys.D)
+                    {
+                        lock (_LBlocks)
+                        {
+                            _LBlocks.Add(new DrunkBlock(p));
+                        }
+                    }
+
+                    if(input == Keys.C)
+                    {
+                        lock (_LBlocks)
+                        {
+                            _LBlocks.Add(new ColourBlock(p));
+                        }
+                    }
+
+                    if(input == Keys.Escape)
+                    {
+                        lock (_LBlocks)
+                        {
+                            _LBlocks.RemoveAll((b) => b.Outside);
+                        }
+                    }
+
+                    if(input == Keys.F1)
+                    {
+                        lock (_LBlocks)
+                        {
+                            _LBlocks.Clear();
+                        }
+                    }
+                    input = Keys.None;
+
+                }
+
+                Thread.Sleep(50);
+            }
+        }
+
+        private void _pCanvas_KeyboardEvent(bool bIsDown, Keys keyCode, CDrawer dr)
+        {
+            if (bIsDown)
+            {
+                input = keyCode;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            input = e.KeyCode;
+        }
+
+        
+    }
+
+    
+}
